c6cd6703d1df20e0184182212fab8f63a62572c7
Felipe Schorles <schorles21@gmail.com>
2020-02-16 20:32:47 -0300

    Sprint-01

---
+++ index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://en.wikipedia.org/wiki/Coronavirus');
+  await page.screenshot({path: 'wiki.png'});
+
+    const result = await page.evaluate(() => {
+        let headingFromWeb = document.querySelectorAll(".mw-headline");
+        const headingList = [...headingFromWeb];
+        return headingList.map(h => h.innerText);
+    });
+
+    console.log(result);
+
+  await browser.close();
+})();
