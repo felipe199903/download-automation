462e725f0be1078d4bc36d9c922040105378f49a
Felipe <schorles21@gmail.com>
2020-04-02 22:57:05 -0300

    Sprint-03

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ const puppeteer = require('puppeteer');
   await page.goto('https://www.baixarfilmetorrenthd.com/genero/filmes/?orderby=lancamento');
   await page.screenshot({path: 'download.png'});
 
-  await page.click(".item");
+  const titleMovie = await page.evaluate(() => {
+    let movieTitleHeading = document.querySelectorAll(".titulo"); 
+    const movieTitleList = [...movieTitleHeading];
+    return movieTitleList.map(h => h.innerText);
+});
 
-    const result = await page.evaluate(() => {
-        let headingFromWeb = document.querySelectorAll(".item-titulo"); 
-        const headingList = [...headingFromWeb];
-        return headingList.map(h => h.innerText);
-    });
+const movie = await page.evaluate(() => {
+  let movieHeading = document.querySelectorAll(".item"); 
+  const movieList = [...movieHeading];
+  return movieList.map(h => h.innerText);
+});  
+  await page.click(".item");
 
     const resultLinks = await page.evaluate(() => {
       let links = document.querySelectorAll(".td-mv-dow"); 
       const linksLista = [...links];
       return linksLista.map(h => h.innerHTML);
   });
-    console.log("Titulos:");
-    console.log(result);
+
+  const hrefs = await page.$$eval('td-mv-dow', as => as.map(a => a.href));
+
+    console.log("Title Movies:");
+    console.log(titleMovie);
+
+    console.log("Movies:");
+    console.log(movie);
+
+    console.log("Links_page:");
+    console.log(hrefs);
 
     console.log("Links:");
     console.log(resultLinks);
